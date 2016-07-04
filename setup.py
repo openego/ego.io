ed0e0db19c8bf14d5e58507234ad16497df7e79e
gplessm <guido.plessmann@rl-institut.de>
2016-07-04 16:31:06 +0200

    Change name to make import feasible

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import find_packages, setup
 
-setup(name='ego.io',
+setup(name='egoio',
       author='openego development group',
       description='ego input/output repository',
       packages=find_packages()
