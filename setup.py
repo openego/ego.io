82859bc1ca1999177327f6ffae3389d23ee2982a
gplessm <guido.plessmann@rl-institut.de>
2016-07-21 11:54:17 +0200

    Update septup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import find_packages, setup
 
 setup(name='ego.io',
       author='openego development group',
+      author_email='oemof@rl-institut.de',
       description='ego input/output repository',
+      version='0.0.1-pre',
+      url='https://github.com/openego/ego.io',
       packages=find_packages()
      )
