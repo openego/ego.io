b0f6d53757f04411b67faf5a877bc63869aec08f
Jonathan Amme <jonathan.amme@rl-institut.de>
2016-08-05 00:17:40 +0200

    add table calc_ego_re.ego_deu_dea to defs

---
+++ egoio/db_tables/calc_ego_re.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+from sqlalchemy import BigInteger, Column, Float, Integer, SmallInteger, String, \
+    Table, Text, text, Numeric
+from geoalchemy2.types import Geometry
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class EgoDeuDea(Base):
+    __tablename__ = 'ego_deu_dea'
+    __table_args__ = {'schema': 'calc_ego_re'}
+
+    id = Column(Integer, primary_key=True)
+    sort = Column(Integer)
+    electrical_capacity = Column(Numeric)
+    generation_type = Column(Text)
+    generation_subtype = Column(String)
+    voltage_level = Column(String)
+    voltage_type = Column(String(2))
+    subst_id = Column(Integer)
+    la_id = Column(Integer)
+    geom_line = Column(Geometry('LINESTRING', 3035), index=True)
+    geom = Column(Geometry('POINT', 3035), index=True)
+    geom_new = Column(Geometry('POINT', 3035), index=True)
+    flag = Column(String)
