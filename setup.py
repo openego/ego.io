b85dce2271d0d0790922fc85bc8695f7e266930c
guido <guido.plessmann@rl-institut.de>
2016-10-14 11:52:14 +0200

    Fix dependency specification

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(name='ego.io',
       author='openego development group',
       author_email='oemof@rl-institut.de',
       description='ego input/output repository',
-      version='0.0.1rc2',
+      version='0.0.1rc3',
       url='https://github.com/openego/ego.io',
       packages=find_packages(),
       install_requires=[
           'geoalchemy2',
-          "sqlalchemy['postgresql']",
+          "sqlalchemy[postgresql]",
           "numpy",
           "psycopg2"]
       )
