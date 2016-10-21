580bd8c2d478f67b6bdad248457f4e2dde102084
gplessm <guido.plessmann@rl-institut.de>
2016-10-21 14:29:25 +0200

    Correct extras_require

--- setup.py
+++ setup.py
@@ -7,13 +7,16 @@ setup(name='ego.io',
       author='openego development group',
       author_email='oemof@rl-institut.de',
       description='ego input/output repository',
-      version='0.0.1rc4',
+      version='0.0.1rc5',
       url='https://github.com/openego/ego.io',
       packages=find_packages(),
       license='GNU GENERAL PUBLIC LICENSE Version 3',
       install_requires=[
           'geoalchemy2',
-          "sqlalchemy[postgresql] <=1.0.15",
+          "sqlalchemy <=1.0.15",
           "numpy",
-          "psycopg2"]
+          "psycopg2"],
+      extras_require={
+          "sqlalchemy <=1.0.15": 'postgresql'
+      }
       )
