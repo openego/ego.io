4a0e690f54d6dbf2c7dac95260e028810244fd66
ulfmueller <ulf.p.mueller@fh-flensburg.de>
2016-10-26 13:40:26 +0200

    Update calc_ego_loads.py
    delete .base in order to be compatible with newest version of sqlalchemy 1.1.2

--- egoio/db_tables/calc_ego_loads.py
+++ egoio/db_tables/calc_ego_loads.py
@@ -3,7 +3,7 @@ from sqlalchemy import BigInteger, Column, Float, Integer, Numeric, String, \
     Table, text, ForeignKey
 from geoalchemy2.types import Geometry
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.dialects.postgresql.base import ARRAY
+from sqlalchemy.dialects.postgresql import ARRAY
 
 Base = declarative_base()
 metadata = Base.metadata
