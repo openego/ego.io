74564d5700c1fffa32db62d04d45a16a750fd255
gplessm <guido.plessmann@rl-institut.de>
2016-11-28 13:01:39 +0100

    Add table definitions

--- egoio/db_tables/calc_ego_re.py
+++ egoio/db_tables/calc_ego_re.py
@@ -25,3 +25,22 @@ class EgoDeuDea(Base):
     geom = Column(Geometry('POINT', 3035), index=True)
     geom_new = Column(Geometry('POINT', 3035), index=True)
     flag = Column(String)
+
+
+class EgoDeaAllocation(Base):
+    __tablename__ = 'ego_dea_allocation'
+    __table_args__ = {'schema': 'model_draft'}
+
+    id = Column(Integer, primary_key=True)
+    sort = Column(Integer)
+    electrical_capacity = Column(Numeric)
+    generation_type = Column(Text)
+    generation_subtype = Column(String)
+    voltage_level = Column(String)
+    postcode = Column(String)
+    subst_id = Column(Integer)
+    la_id = Column(Integer)
+    geom_line = Column(Geometry('LINESTRING', 3035), index=True)
+    geom = Column(Geometry('POINT', 3035), index=True)
+    geom_new = Column(Geometry('POINT', 3035), index=True)
+    flag = Column(String)
