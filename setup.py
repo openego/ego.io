aa7ea0712ab4f5f5f9893f71125b1ab38e04866a
gplessm <guido.plessmann@rl-institut.de>
2016-12-19 15:34:28 +0100

    Avoid package name conflicts

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 
 from setuptools import find_packages, setup
 
-setup(name='ego.io',
+setup(name='egoio',
       author='openego development group',
       author_email='oemof@rl-institut.de',
       description='ego input/output repository',
-      version='0.0.1',
+      version='0.0.2',
       url='https://github.com/openego/ego.io',
       packages=find_packages(),
       license='GNU GENERAL PUBLIC LICENSE Version 3',
