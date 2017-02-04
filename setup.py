aa2913ed3ccec2813af36d1718c63a960149f672
Jonathan Amme <jonathan.amme@rl-institut.de>
2017-02-04 15:37:35 +0100

    fix bug, modify authors of repo in setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@
 
 from setuptools import find_packages, setup
 
-setup(name='ego.io',
-      author='openego development group',
-      author_email='oemof@rl-institut.de',
+setup(name='egoio',
+      author='NEXT ENERGY, Reiner Lemoine Institut gGmbH, ZNES',
+      author_email='',
       description='ego input/output repository',
-      version='0.0.1rc7',
+      version='0.0.2',
       url='https://github.com/openego/ego.io',
       packages=find_packages(),
       license='GNU GENERAL PUBLIC LICENSE Version 3',
       install_requires=[
-          'geoalchemy2',
-          "sqlalchemy",
-          "numpy",
-          "psycopg2"],
+          'geoalchemy2 >= 0.3.0, <=0.3.0',
+          'sqlalchemy >= 1.0.15, <= 1.1.4',
+          'numpy >= 1.11.2, <= 1.11.2',
+          'psycopg2'],
       extras_require={
-          "sqlalchemy": 'postgresql'
-      }
+          "sqlalchemy": 'postgresql'}
       )
