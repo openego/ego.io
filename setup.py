9edc751580ecf1a8c4980314754d645dcb6d69cb
gplessm <guido.plessmann@rl-institut.de>
2018-01-14 00:09:41 +0100

    Add keyrings.alt
    Required to use keyring on Xubuntu 17.04

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name='egoio',
           'geoalchemy2 >= 0.3.0, <= 0.4.1',
           'sqlalchemy >= 1.0.11, <= 1.2.0',
           'keyring >= 4.0',
+          'keyrings.alt',
           'psycopg2'],
       extras_require={
           "sqlalchemy": 'postgresql'},
