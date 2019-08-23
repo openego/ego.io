f73be516f508837f8f8b920777e0ba9c872f2f5e
nesnoj <jonathan.amme@rl-institut.de>
2019-08-23 14:33:39 +0200

    include oedialect in package requirements #70

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(name='egoio',
           'sqlalchemy >= 1.2.0',
           'keyring >= 4.0',
           'keyrings.alt',
-          'psycopg2'],
+          'psycopg2',
+          'oedialect'],
       extras_require={
           "sqlalchemy": 'postgresql'},
       package_data={'tools': ['sqlacodegen_oedb.sh']}
